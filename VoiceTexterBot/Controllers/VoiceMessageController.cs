faf1b15bf9695d467970ce41667818a3b77f4440
JJibbz <vkazancev322@gmail.com>
2024-02-08 22:40:04 +0700

    Добавьте файлы проекта.

---
+++ VoiceTexterBot/Controllers/VoiceMessageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using VoiceTexterBot.Configuration;
+using VoiceTexterBot.Services;
+
+namespace VoiceTexterBot.Controllers
+{
+    public class VoiceMessageController
+    {
+        private readonly AppSettings _appSettings;
+        private readonly ITelegramBotClient _telegramClient;
+        private readonly IFileHandler _audioFileHandler;
+        private readonly IStorage _memoryStorage;
+
+        public VoiceMessageController(AppSettings appSettings, ITelegramBotClient telegramBotClient, IFileHandler audioFileHandler, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _appSettings = appSettings;
+            _audioFileHandler = audioFileHandler;
+            _memoryStorage = memoryStorage;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            var fileId = message.Voice?.FileId;
+            if (fileId == null)
+                return;
+
+            await _audioFileHandler.Download(fileId, ct);
+
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Голосовое сообщение загружено", cancellationToken: ct);
+            
+            string userLanguageCode = _memoryStorage.GetSession(message.Chat.Id).LanguageCode; //Здесь получим язык из сессии пользователя
+            _audioFileHandler.Process(userLanguageCode); //Запустим обработку
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Голосовое сообщение конвертировано в формат .WAV", cancellationToken: ct);
+        }
+    }
+}
